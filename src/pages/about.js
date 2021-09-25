bada1933178073887799be24ff9e2415b96001e7
japneet0411 <60810523+japneet0411@users.noreply.github.com>
2021-09-25 12:42:26 +0530

    project section added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar";
-import Lottie from "react-lottie";
+import me from "./../images/me.jpg";
 import { FaDownload, FaGithub, FaLinkedin } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
-import * as animationData from "./../images/girl2.json";
+import { GrMail } from "react-icons/gr";
+
 import resume from "./../images/resume.pdf";
 const About = () => {
-  const defaultOptions = {
-    hover: false,
-    loop: true,
-    autoplay: true,
-    speed: "1.5",
-    onclick: false,
-    controls: false,
-    animationData: animationData,
-    background: "transparent",
-
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
   return (
     <div className="bg-pattern h-full md:h-screen ">
       <Navbar />
@@ -28,20 +15,7 @@ const About = () => {
       <div className="mx-5 flex flex-col md:flex-row mt-7  text-white ">
         <div className="h-screen md:h-auto w-auto md:w-1/2 mx-auto lg:ml-10 lg:mr-1 space-y-5 ">
           <p className="rounded-xl ease-in-out duration-500 transform hover:scale-105 border-2 border-white p-3  flex-wrap text-lg ">
-            <Lottie
-              options={defaultOptions}
-              height={"250px"}
-              width={"200px"}
-              isClickToPauseDisabled={true}
-              hover={false}
-              className="animation"
-              style={{
-                cursor: "context-menu",
-                margin: "-10px",
-                position: "relative",
-                float: "left",
-              }}
-            />
+            <img src={me} className="float-left me mr-3 mt-3 "></img>
             BTech CSE student (2023 batch) with specialization in IOT.
             Interested in web development and competitive programming. Hard
             working and determined, open to learning new technologies, with good
@@ -49,20 +23,21 @@ const About = () => {
             Member of IEEE computer Society, IoThinc society of VIT, Vellore.
             <br />
             <br />
-            <div className="text-center">
+            <div className="ml-3">
               <a
                 href={resume}
                 download
-                className=" hover:bg-white hover:text-black mx-auto cursor-pointer border-2 border-white mt-12 p-1 px-4"
+                className=" hover:bg-white hover:text-black mx-auto cursor-pointer border-2 border-white mt-9 p-1 px-4"
               >
                 RESUME
                 <FaDownload className="inline-block ml-5 bg-transparent border-transparent" />
               </a>
+              <hr className="bg-white mt-3 "></hr>
             </div>
-            <p className="text-center text-xl mt-6">GET IN TOUCH ☎</p>
-            <p className="text-center mt-3 space-y-3 ">
+            <p className="text-center text-xl mt-2">GET IN TOUCH </p>
+            <p className="text-center mt-1 space-y-2 ">
               Phone: +91 9891464111 <br />
-              Gmail: japneet0411@gmail.com
+              Email: japneet0411@gmail.com
             </p>
             <p className="mt-5 space-x-5 mx-auto w-32 mx-auto flex flex-row ">
               <AiFillInstagram className="cursor-pointer" size={25} />
